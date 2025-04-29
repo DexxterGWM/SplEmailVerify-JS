9dfb80d25e7a7b56adc59b0573911ccc7b5fd34b
DexxterGWM <99261879+DexxterGWM@users.noreply.github.com>
2025-04-29 18:54:58 -0300

    script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,18 @@
+const Form = document.getElementById('EmailForm');
+const EmailInput = document.getElementById('EmailForm_Email');
+const Message = document.getElementById('EmailForm_Message');
 
+Form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const EmailValue = EmailInput.value;
+    const EmailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!EmailPattern.test(EmailValue)) {
+        Message.textContent = 'Por favor, insira um e-mail válido. Exemplo: exemplo@dominio.com';
+        Message.style.color = 'red';
+    } else {
+        Message.textContent = 'E-mail válido!';
+        Message.style.color = 'green';
+    }
+});
